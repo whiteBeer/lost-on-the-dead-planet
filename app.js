3a39d712dd38da898b8ade0d11a7694719996f82
syeskov <sjjj@mail.ru>
2024-02-06 11:38:17 +0300

    diff colors

--- app.js
+++ app.js
@@ -7,27 +7,54 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+const colors = ["red", "blue", "green", "#8F3A84", "#212121"];
+let players = [];
+
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'client/index.html'));
 });
 
 io.on('connection', (socket) => {
-    console.log("Connected: " + socket.id);
+
+    const playersColors = players.map(el => el.color);
+    const color = colors.find(el => {
+        return playersColors.indexOf(el) === -1;
+    });
+    players.push({
+        socketId: socket.id,
+        color: color,
+    });
+
+    console.log("Connected: " + socket.id, players);
 
     socket.on('playerMoved', (params) => {
-        console.log(params);
-        io.emit('playerMoved', {
-            ...params,
-            socketId: socket.id
+        let currentPlayerIndex = -1;
+        players = players.map((el, i) => {
+            if (el.socketId === socket.id) {
+                currentPlayerIndex = i;
+                return {
+                    ...el,
+                    pageX: params.pageX,
+                    pageY: params.pageY
+                };
+            } else {
+                return el;
+            }
         });
+        if (currentPlayerIndex !== -1) {
+            io.emit('playerMoved', players[currentPlayerIndex]);
+        }
     });
 
     socket.on('disconnect', () => {
+        players = players.filter(it => it.socketId !== socket.id);
         io.emit('userDisconnected', {
             socketId: socket.id
         });
-        console.log('user disconnected');
+        console.log('Disconnected ', socket.id, players);
     });
+
+    io.emit('allPlayers', players);
 });
 
 server.listen(3000, () => {
