15edef5928e9d10e5863d664c91c744d9925b2f7
syeskov <sjjj@mail.ru>
2026-02-23 12:15:50 +0300

    Github CI/CD

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -24,7 +24,7 @@ function Main() {
 
     return (
         <div className="flex flex-col items-center mt-10 text-xl font-bold">
-            Lost on the dead Planet
+            Lost on the dead planet
             <Button className="mt-3" onClick={joinRoom}>Join Game</Button>
             <Button className="mt-3" onClick={createRoom}>Create Game</Button>
         </div>
